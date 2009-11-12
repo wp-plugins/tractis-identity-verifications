af92976889b80e4e735bfeae8d2894b658b48d22
Tractis <Tractis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-12 12:25:54 +0000

    Alert site admins if role is different to suscriber on plugin configuration page
    git-svn-id: https://plugins.svn.wordpress.org/tractis-identity-verifications/trunk@172619 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tractis_ui.php
+++ tractis_ui.php
@@ -10,6 +10,15 @@
 		global $tractis_auth_settings;
 		
 		$message = "";
+
+		// Review default role on user creation to show a warning to the blog-admin
+		$default_role = get_option('default_role');
+		
+		if ($default_role != "subscriber")
+		{
+			$message .= __("Warning: The default role for new users in your Wordpress config is: ", "tractis_auth").$default_role."<br /><br />"; 
+			$message .= __(" * We recommend that you use the suscriber role or other with read permissions (Settings->New User Default Role), <a href='http://codex.wordpress.org/Roles_and_Capabilities'>read more here</a>.", "tractis_auth")."<br />";
+		}	
 		
 	    if (isset($_POST['tractis_auth_updateSettings'])) {
 	        foreach ($tractis_auth_settings as $setting => $value) {
@@ -21,9 +30,9 @@
 	        }
 	        
 	        if (!$_POST['tractis_auth_api_key'] || $_POST['tractis_auth_api_key'] == "") {
-	        	$message = "Settings Updated. (Warning, the api key must be filled to work LINK A LA HELP)";
+	        	$message .= __("Settings Updated. (Warning, the api key must be filled to work LINK A LA HELP)", "tractis_auth");
 	        } else {
-	        	$message = "Settings Updated.";
+	        	$message .= __("Settings Updated.", "tractis_auth");
 	        }
 	    }
 	    require_once (TRACTIS_AUTH_PLUGIN_VIEWS . '/ui_settings.php'); 	
